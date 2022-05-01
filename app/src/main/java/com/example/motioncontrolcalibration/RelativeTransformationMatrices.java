ad8789d58023c8b7b5f7f86ce7c55b51c8e151aa
Viral Panchal <viralpanchal1999@gmail.com>
2022-05-01 00:53:44 -0400

    Rotation Matrices print correctly

--- app/src/main/java/com/example/motioncontrolcalibration/RelativeTransformationMatrices.java
+++ app/src/main/java/com/example/motioncontrolcalibration/RelativeTransformationMatrices.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.InputStream;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 import jxl.Cell;
@@ -34,10 +35,11 @@ public class RelativeTransformationMatrices extends AppCompatActivity {
         start_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                System.out.println("Relative Transformation Matrix here ");
                 read_data_orientation_z();
                 action_textview = (TextView)findViewById(R.id.RZRYRX_matrix);
                 //action_textview.setText();///////////////////////////////////////NEED Attention
-                System.out.println("Relative Transformartion Matrix here ");
+
             }
         });
     }
@@ -48,7 +50,7 @@ public class RelativeTransformationMatrices extends AppCompatActivity {
             InputStream is = am.open("Xsens DOT_Pelvis_Orientation.xls");
             Workbook wb = Workbook.getWorkbook(is);
             Sheet s = wb.getSheet(0);
-            System.out.println("Data read");
+
             ArrayList<Double> ang_x;
             ArrayList<Double> ang_y;
             ArrayList<Double> ang_z;
@@ -115,34 +117,88 @@ public class RelativeTransformationMatrices extends AppCompatActivity {
                 cos_ang_z.add(cos_z);
             }
 
+
             for (int j = 0; j < sin_ang_x.size(); j++) {
                 Rx = new double[][]{{1, 0, 0}, {0, cos_ang_x.get(j), (-1 * sin_ang_x.get(j))}, {0, sin_ang_x.get(j), cos_ang_x.get(j)}};
                 Ry = new double[][]{{cos_ang_y.get(j),0,sin_ang_y.get(j)},{0,1,0},{(-1*sin_ang_y.get(j)),0,cos_ang_y.get(j)}};
                 Rz = new double[][]{{cos_ang_z.get(j),(-1*sin_ang_z.get(j)),0},{sin_ang_z.get(j),cos_ang_z.get(j),0},{0,0,1}};
 
-                /// FOR RX*RY
-                for (int i = 0; i < 3; i++) {
-                        RzRy[i][0] = (Rz[i][0] * Ry[0][i]) + (Rz[i][1] * Ry[1][i]) + (Rz[i][2] * Ry[2][i]);
-                        RzRy[i][1] = (Rz[i][0] * Ry[i][1]) + (Rz[i][1] * Ry[1][1]) + (Rz[i][2] * Ry[2][2]);
-                        RzRy[i][2] = (Rz[i][0] * Ry[0][2]) + (Rz[i][1] * Ry[1][2]) + (Rz[i][2] * Ry[2][2]);
 
-                    for (int k = 0; k < 3; k++) {
-                        System.out.print(RzRy[i][k] + " ");
+                final DecimalFormat df = new DecimalFormat("0.0000");
+                System.out.println("Rx:");
+                for (int i = 0; i < Rx.length; i++) {
+                    for (int k = 0; k < Rx[i].length; k++) {
+                        System.out.print(df.format(Rx[i][k]) + " ");
                     }
-                    System.out.print("\n");
+                    System.out.println();
                 }
+
+                System.out.println("Ry:");
+                for (int i = 0; i < Ry.length; i++) {
+                    for (int k = 0; k < Ry[i].length; k++) {
+                        System.out.print(df.format(Ry[i][k]) + " ");
+                    }
+                    System.out.println();
+                }
+
+                System.out.println("Rz:");
+                for (int i = 0; i < Rz.length; i++) {
+                    for (int k = 0; k < Rz[i].length; k++) {
+                        System.out.print(df.format(Rz[i][k]) + " ");
+                    }
+                    System.out.println();
+                }
+
+                /// FOR RX*RY
+                double[][] RzRy = new double[3][3];
+                System.out.println("RzRy: ");
+                for (int i = 0; i < 3; i++) {
+                    for (int k = 0; k < 3; k++) {
+                        RzRy[i][k] = 0;
+                        for (int l = 0; l < 3; l++) {
+                            RzRy[i][k] +=Rz[i][l] * Ry[l][j];
+                        }
+                        System.out.print(df.format(RzRy[i][k]) + " ");
+                    }
+                    System.out.println();
+                }
+
+//                for (int i = 0; i < 3; i++) {
+//                        RzRy[i][0] = (Rz[i][0] * Ry[0][i]) + (Rz[i][1] * Ry[1][i]) + (Rz[i][2] * Ry[2][i]);
+//                        RzRy[i][1] = (Rz[i][0] * Ry[i][1]) + (Rz[i][1] * Ry[1][1]) + (Rz[i][2] * Ry[2][2]);
+//                        RzRy[i][2] = (Rz[i][0] * Ry[0][2]) + (Rz[i][1] * Ry[1][2]) + (Rz[i][2] * Ry[2][2]);
+//
+//                    for (int k = 0; k < 3; k++) {
+//                        System.out.print(RzRy[i][k] + " ");
+//                    }
+//                    System.out.print("\n");
+//                }
+                System.out.println("Data read");
 
                 /// FOR RX*RY*RZ
+                double[][] RzRyRx = new double[3][3];
+                System.out.println("RzRyRx: ");
                 for (int i = 0; i < 3; i++) {
-                    RzRyRx[i][0] = (RzRy[i][0] * Rx[0][i]) + (RzRy[i][1] * Rx[1][i]) + (RzRy[i][2] * Rx[2][i]);
-                    RzRyRx[i][1] = (RzRy[i][0] * Rx[i][1]) + (RzRy[i][1] * Rx[1][1]) + (RzRy[i][2] * Rx[2][2]);
-                    RzRyRx[i][2] = (RzRy[i][0] * Rx[0][2]) + (RzRy[i][1] * Rx[1][2]) + (RzRy[i][2] * Rx[2][2]);
-
                     for (int k = 0; k < 3; k++) {
-                        System.out.print(RzRyRx[j][k] + " ");
+                        RzRyRx[i][k] = 0;
+                        for (int l = 0; l < 3; l++) {
+                            RzRyRx[i][k] +=RzRy[i][l] * Ry[l][j];
+                        }
+                        System.out.print(df.format(RzRyRx[i][k]) + " ");
                     }
-                    System.out.print("\n");
+                    System.out.println();
                 }
+//                for (int i = 0; i < 3; i++) {
+//                    RzRyRx[i][0] = (RzRy[i][0] * Rx[0][i]) + (RzRy[i][1] * Rx[1][i]) + (RzRy[i][2] * Rx[2][i]);
+//                    RzRyRx[i][1] = (RzRy[i][0] * Rx[i][1]) + (RzRy[i][1] * Rx[1][1]) + (RzRy[i][2] * Rx[2][2]);
+//                    RzRyRx[i][2] = (RzRy[i][0] * Rx[0][2]) + (RzRy[i][1] * Rx[1][2]) + (RzRy[i][2] * Rx[2][2]);
+//
+//                    for (int k = 0; k < 3; k++) {
+//                        System.out.print(RzRyRx[j][k] + " ");
+//                    }
+//                    System.out.print("\n");
+//                }
+                Thread.sleep(2000);
             }
 
         } catch (Exception e){
